5d64d0c8077106e93e7b40d2ba00377bd2c0d26a
MULTIVISION <multivision@MULTIVISIONs-MacBook-Pro.local>
2019-04-04 13:01:11 +0200

    Setting vue-cli and add components

---
+++ vue-cli-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+          <server-header></server-header>
+        <hr>
+        <div class="row">
+            <server-list></server-list>
+            <div class="col-xs-12 col-sm-6">
+                <server-details></server-details>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <server-footer></server-footer>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+import ServerList from './ServerList.vue';
+import ServerDetails from './ServerDetails.vue';
+
+export default {
+  components: {
+      'server-header': Header,
+      'server-footer': Footer,
+      'server-list': ServerList,
+      'server-details':ServerDetails
+  }
+}
+</script>
+
