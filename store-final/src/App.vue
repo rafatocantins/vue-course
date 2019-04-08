523a2cba8fbf02d1a6e5f5383676c38c80419c2a
MULTIVISION <multivision@MULTIVISIONs-MacBook-Pro.local>
2019-04-08 16:45:44 +0200

    add animations to routes

--- store-final/src/App.vue
+++ store-final/src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <div class="container-fluid">
       <appHeader></appHeader>
-              <router-view></router-view>
+      <!-- Add animations between routes -->
+        <transition name="slide" mode="out-in">
+            <router-view></router-view>
+        </transition>
     </div>
   </div>
 </template>
@@ -27,5 +30,36 @@ export default {
 <style lang="scss">
 body{
   padding: 30px;
+}
+
+/** add Animations **/
+
+.slide-enter-active{
+  animation: slide-in 300ms ease-out forwards; 
+}
+.slide-leave-active{
+  animation: slide-out 300ms ease-out forwards;
+}
+
+@keyframes slide-in{
+  from{
+    transform: rotateY(-90deg);
+    opacity:0
+  }
+  to{
+    transform: rotateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out{
+  from{
+    transform: rotateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: rotateY(-90deg);
+    opacity: 0;
+  }
 }
 </style>
